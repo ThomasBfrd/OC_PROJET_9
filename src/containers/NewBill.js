ca020a8c95ec9b7391153ed06f19c246d438c12b
ThomasBfrd <thomas.bfrd@gmail.com>
2024-07-19 15:03:17 +0200

    Fix extension file + submit btn disabled or not

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -15,32 +15,58 @@ export default class NewBill {
     this.billId = null
     this.isWrongFile = false;
     new Logout({ document, localStorage, onNavigate })
+
+    // Appel de la méthode qui écoute tous les inputs requis et qui désactive le btn submit
+    this.handleWaitingForm();
   }
+
+  handleWaitingForm = () => {
+
+    const form = this.document.querySelector('#form-new-bill')
+    const submitBtn = this.document.querySelector('#btn-send-bill')
+    const requiredFields = form.querySelectorAll("[required]")
+
+    const checkForm = () => {
+      if (form.checkValidity()) {
+        submitBtn.disabled = false;
+      } else {
+        submitBtn.disabled = true;
+      }
+    };
+    requiredFields.forEach(field => {
+      field.addEventListener("input", checkForm);
+    });
+  }
+
   handleChangeFile = e => {
-    e.preventDefault()
+    e.preventDefault();
     let errorFile = this.document.querySelector('#file-error-message');
     errorFile.innerHTML = "";
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-
+    const file = this.document.querySelector(`input[data-testid="file"]`).files[0];
+    const filePath = e.target.value.split(/\\/g);
+    const fileName = filePath[filePath.length-1];
+  
     // On autorise seulement ces extensions
     const allowedExtensions = ['jpg', 'jpeg', 'png'];
-    // On affiche l'extension du fichier
-    const fileExtension = fileName.slice((fileName.lastIndexOf(".") - 1 >>> 0) + 2).toLowerCase();
 
+    // Récupération de l'extension du fichier uploadé
+    const fileExtension = fileName.split('.')[1];
+
+    // On récupère le bouton du DOM pour le désactiver si le type de fichier est mauvais
+    const button = this.document.querySelector('#btn-send-bill');
+  
     if (!allowedExtensions.includes(fileExtension)) {
-      // alert("Seuls les fichiers jpg, jpeg et png sont autorisés.");
-      errorFile.innerHTML = "Fichier non compatible, seuls les fichiers jpg, jpeg et png sont autorisés."
+      errorFile.innerHTML = "Fichier non compatible, seuls les fichiers jpg, jpeg et png sont autorisés.";
       this.isWrongFile = true;
-      return;
-    }
-
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
-
+      button.disabled = true;
+    } else {
+      errorFile.innerHTML = "";
+      button.disabled = false;
+      const formData = new FormData();
+      const email = JSON.parse(localStorage.getItem("user")).email;
+      formData.append('file', file);
+      formData.append('email', email);
+  
       this.store
         .bills()
         .create({
@@ -50,11 +76,15 @@ export default class NewBill {
           }
         })
         .then(({fileUrl, key}) => {
-          console.log(fileUrl)
-          this.billId = key
-          this.fileUrl = fileUrl
-          this.fileName = fileName
-        }).catch(error => console.error(error))
+          console.log(fileUrl);
+          this.billId = key;
+          this.fileUrl = fileUrl;
+          this.fileName = fileName;
+          this.isWrongFile = false;
+        }).catch(error => {
+          console.error(error);
+        });
+    }
   }
   handleSubmit = e => {
     e.preventDefault()
@@ -77,11 +107,7 @@ export default class NewBill {
     if(!this.isWrongFile) {
       this.updateBill(bill)
       this.onNavigate(ROUTES_PATH['Bills'])
-      // Sinon, on affiche un popup d'erreur et on redirige le user vers ses notes de frais
-    } else {
-      alert("Seuls les fichiers jpg, jpeg et png sont autorisés.");
-    }
-  }
+  }}
 
   // not need to cover this function by tests
   updateBill = (bill) => {
